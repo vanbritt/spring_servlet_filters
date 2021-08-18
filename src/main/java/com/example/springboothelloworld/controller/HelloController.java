f67ee51d5f418d25e23f34dd0be4ed290ed9aeab
bricetangang <brice.tangang@dembou-consulting.com>
2021-08-18 08:23:40 +0100

    Servlet, Filters and Listeners

---
+++ src/main/java/com/example/springboothelloworld/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.springboothelloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+    private Map<String, Object> result = new HashMap<>();
+
+    @RequestMapping("/hello")
+    public Map<String,Object> getResult(){
+        result.put("name", "Brice");
+        result.put("city", "Douala");
+
+        return  result;
+    }
+
+}
